f8ebe79135c5432116fdf49a3d6835dae4b16d89
Andrew <andrew@lobos.me>
2018-03-12 11:51:21 -0400

    Better error handling

--- verifier.rb
+++ verifier.rb
@@ -10,32 +10,43 @@ if ARGV.size !=1
 end
 blockchain = Blockchain.new
   lineNum = 0
-File.open(ARGV[0], "r").each_line do |line|
-    blockText = line.chomp!.split("|")
-    if blockText.length == 5
-        timeText = blockText[3].split(".")
-        sec = timeText[0].to_i
-        nanoseconds = timeText[1].to_i
-
-        block = Block.new blockText[0].to_i, blockText[1],blockText[2], sec, nanoseconds, blockText[4]
-        blockText[2].split(":").each do | transactionText |
-            transactionInfo = /(.*)>(.*)\(([0-9]*)\)/.match(transactionText)
-            fromWallet = blockchain.getWallet transactionInfo[1]
-            toWallet = blockchain.getWallet transactionInfo[2]
-
-            transaction = Transaction.new fromWallet, toWallet, transactionInfo[3].to_f
-            block.addTransaction transaction
-        end
-
+begin
+    File.open(ARGV[0], "r").each_line do |line|
         begin
-            blockchain.applyBlock block,lineNum
-        rescue Exception => e
-            puts e.message
+            blockText = line.chomp!.split("|")
+            if blockText.length == 5
+                timeText = blockText[3].split(".")
+                sec = timeText[0].to_i
+                nanoseconds = timeText[1].to_i
+
+                block = Block.new blockText[0].to_i, blockText[1],blockText[2], sec, nanoseconds, blockText[4]
+                blockText[2].split(":").each do | transactionText |
+                    transactionInfo = /(.*)>(.*)\(([0-9]*)\)/.match(transactionText)
+                    fromWallet = blockchain.getWallet transactionInfo[1]
+                    toWallet = blockchain.getWallet transactionInfo[2]
+
+                    transaction = Transaction.new fromWallet, toWallet, transactionInfo[3].to_f
+                    block.addTransaction transaction
+                end
+
+                begin
+                    blockchain.applyBlock block,lineNum
+                rescue StandardError => e
+                    puts e.message
+                    puts "BLOCKCHAIN INVALID"
+                    exit(1)
+                end
+                lineNum+=1
+            end
+        rescue StandardError => e
+            puts "Line #{lineNum}: Block format invalid on line"
             puts "BLOCKCHAIN INVALID"
             exit(1)
         end
-        lineNum+=1
     end
+rescue StandardError => e
+    puts "Failed to open file"
+    exit(1)
 end
 
 blockchain.wallets.each do |key, wallet|
