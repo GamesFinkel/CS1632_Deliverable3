9f401e6d277985f251d0d0162752f4c10d8df222
James Finkel <32717956+jaf177@users.noreply.github.com>
2018-03-14 01:10:44 -0400

    Minor formatting in verifier

--- verifier.rb
+++ verifier.rb
@@ -1,6 +1,3 @@
-#TODO Check argv length
-#TODO check length after splits
-
 require_relative "Blockchain"
 require_relative "Block"
 require_relative "Transaction"
@@ -9,46 +6,41 @@ if ARGV.size !=1
   exit()
 end
 blockchain = Blockchain.new
-  lineNum = 0
+lineNum = 0
 begin
-    File.open(ARGV[0], "r").each_line do |line|
-        begin
-            blockText = line.chomp!.split("|")
-            timeText = blockText[3].split(".")
-            sec = timeText[0].to_i
-            nanoseconds = timeText[1].to_i
-
-            block = Block.new blockText[0].to_i, blockText[1],blockText[2], sec, nanoseconds, blockText[4]
-            blockText[2].split(":").each do | transactionText |
-                transactionInfo = /(.*)>(.*)\(([0-9]*)\)/.match(transactionText)
-                fromWallet = blockchain.getWallet transactionInfo[1]
-                toWallet = blockchain.getWallet transactionInfo[2]
-
-                transaction = Transaction.new fromWallet, toWallet, transactionInfo[3].to_f
-                block.addTransaction transaction
-            end
-
-            begin
-                blockchain.applyBlock block,lineNum
-            rescue StandardError => e
-                puts e.message
-                puts "BLOCKCHAIN INVALID"
-                exit(1)
-            end
-
-            lineNum+=1
-        rescue StandardError => e
-            puts "Line #{lineNum}: Block format invalid on line"
-            puts "BLOCKCHAIN INVALID"
-            exit(1)
-        end
+  File.open(ARGV[0], "r").each_line do |line|
+    begin
+      blockText = line.chomp!.split("|")
+      timeText = blockText[3].split(".")
+      sec = timeText[0].to_i
+      nanoseconds = timeText[1].to_i
+      block = Block.new blockText[0].to_i, blockText[1],blockText[2], sec, nanoseconds, blockText[4]
+      blockText[2].split(":").each do | transactionText |
+        transactionInfo = /(.*)>(.*)\(([0-9]*)\)/.match(transactionText)
+        fromWallet = blockchain.getWallet transactionInfo[1]
+        toWallet = blockchain.getWallet transactionInfo[2]
+        transaction = Transaction.new fromWallet, toWallet, transactionInfo[3].to_f
+        block.addTransaction transaction
+      end
+      begin
+        blockchain.applyBlock block,lineNum
+      rescue StandardError => e
+        puts e.message
+        puts "BLOCKCHAIN INVALID"
+        exit(1)
+      end
+      lineNum+=1
+      rescue StandardError => e
+        puts "Line #{lineNum}: Block format invalid on line"
+        puts "BLOCKCHAIN INVALID"
+        exit(1)
     end
-rescue StandardError => e
+  end
+  rescue StandardError => e
     puts "Failed to open file"
+    puts "BLOCKCHAIN INVALID"
     exit(1)
 end
-
 blockchain.wallets.each do |key, wallet|
-    puts "#{wallet.owner}: #{wallet.balance.to_i} billcoins"
+  puts "#{wallet.owner}: #{wallet.balance.to_i} billcoins"
 end
-
